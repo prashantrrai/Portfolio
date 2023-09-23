0e15da9f55b7406eff68585431642612be52a02a
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-09-24 01:33:09 +0530

    img--> changed to Image and ' changed to &apos;

--- src/app/components/UI/Hero/Hero.tsx
+++ src/app/components/UI/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 // components/Hero.js
 
 import React from 'react';
+import Image from 'next/image';
 
 const Hero = () => {
     const phoneNumber = '7359030960';
@@ -10,15 +11,15 @@ const Hero = () => {
   return (
     <section className="text-white py-16">
       <div className="container mx-auto text-center p-2">
-        <img
-          src="../../Passport copy 50kb-min.jpeg" 
+        <Image
+          src="/Passport copy 50kb-min.jpeg" 
           alt="profile-photo"
           className="mx-auto w-40 h-40 rounded-full mb-4"
         />
-        <h1 className="text-2xl font-bold mb-2">Hello, I'm Prashant Rai</h1>
+        <h1 className="text-2xl font-bold mb-2">Hello, I&apos;m Prashant Rai</h1>
         <p className="text-lg mb-4">MERN Stack Developer</p>
         <p className="text-md mb-8 px-2">
-          Hi there! My name is Piyush Garg and I’m a software engineer with over 5 years of experience in the industry. I love all things tech and coding, and on my channel, I share my knowledge and experience with others.
+          Hi there! My name is Piyush Garg and I&apos;m a software engineer with over 5 years of experience in the industry. I love all things tech and coding, and on my channel, I share my knowledge and experience with others.
         </p>
         <div>
           <a href={whatsappLink} target='_blank' className="bg-white text-blue-500 hover:bg-blue-200 px-4 py-2 rounded-full mr-4">Contact Me</a>
